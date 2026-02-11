d90e2a2a0da22bf88fc93f97dd79bb52096d166e
Макс <maksimas@Mac-mini-Maks.local>
2026-02-11 22:05:24 +0200

    Fix Build: Remove legacy BitmapHelper, adding missing imports

--- app/src/main/java/com/judu/transport/ui/timetable/RouteDetailsScreen.kt
+++ app/src/main/java/com/judu/transport/ui/timetable/RouteDetailsScreen.kt
@@ -1,5 +1,6 @@
 package com.judu.transport.ui.timetable
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
